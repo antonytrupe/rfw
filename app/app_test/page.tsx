185e6d904e57466835f0781db20cd8889b7671b6
TonyT <antony.trupe@gmail.com>
2023-11-12 14:44:55 -0500

    deleted test auth pages

--- app/app_test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../auth"
- 
-export default async function Page() {
-  const session = await getServerSession(authOptions)
-  return <pre>{JSON.stringify(session, null, 2)}</pre>
-}
--- pages/api/pages_api_test.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../auth";
-
-
-export default async function handler(req: any, res: any) {
-  const session = await getServerSession(req, res, authOptions)
-  return res.json({ session })
-}
