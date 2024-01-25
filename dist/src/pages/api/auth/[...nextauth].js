0fac58b29b17747c68d2f02b9728f0cea87fbc4f
TonyT <antony.trupe@gmail.com>
2024-01-24 22:49:06 -0500

    removed cruft

--- dist/src/pages/api/auth/[...nextauth].js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const next_auth_1 = __importDefault(require("next-auth"));
-const auth_1 = require("@/../auth");
-exports.default = (0, next_auth_1.default)(auth_1.authOptions);
